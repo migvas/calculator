7d70066fb7f320384480f005b9b0499b796fd45c
migvas <migas.v94@gmail.com>
2023-03-24 13:50:05 +0000

    feat: Make the calculator responsive
    Add listeners to button and how they interact with the displays

--- script.js
+++ script.js
@@ -1,36 +1,116 @@
 const operations = {
-    add: (a,b) => {
+    add: (a, b) => {
         return a + b;
     },
 
-    subtract: (a,b) => {
+    subtract: (a, b) => {
         return a - b;
     },
 
-    multiply: (a,b) => {
+    multiply: (a, b) => {
         return a * b;
     },
 
-    divide: (a,b) => {
-        if(!b) return 'ERROR';
-        return a/b;
+    divide: (a, b) => {
+        if (!b) {
+            isError = true;
+            return 'ERROR';
+        }
+        return a / b;
     }
 }
 
 const operate = (a, b, operation) => {
 
-    if(!operations[operation]){
+    if (!operations[operation]) {
         console.log('Invalid operation');
+        isError = true;
         return 'ERROR';
     }
     return operations[operation](a, b);
 };
 
-const a = 2;
-const b = 4;
+const helperFunctions = {
+    clear: () => {
+        mainDisplay.textContent = '';
+        secondaryDisplay.textContent = '';
+        prev = '';
+        operation = '';
+        isError = false;
+        isResult = false;
+    },
 
-console.log(operate(a, b, 'add'));
-console.log(operate(a, b, 'subtract'));
-console.log(operate(a, b, 'multiply'));
-console.log(operate(a, b, 'divide'));
-console.log(operate(a, b, 'power'));
+    delete: () => {
+        if (!mainDisplay.textContent || isError) return;
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+    },
+
+    percentage: () => {
+        if (!mainDisplay.textContent || isError) return;
+        mainDisplay.textContent /= 100;
+    }
+}
+
+let prev = '', operation = '';
+let isError = false;
+let isResult = false;
+
+const mainDisplay = document.getElementById('main');
+const secondaryDisplay = document.getElementById('secondary');
+
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if (isResult || isError) {
+            mainDisplay.textContent = number.textContent;
+            secondaryDisplay.textContent = '';
+            isError = false;
+            isResult = false;
+        } else {
+            if (number.id === 'decimal' && mainDisplay.textContent.indexOf('.') !== -1) return;
+
+            mainDisplay.textContent += number.textContent;
+        }
+    })
+});
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        const currentVal = mainDisplay.textContent;
+        if (!currentVal || isError) return;
+
+        if (operator.id === 'equals') {
+            if (isResult) return;
+
+            secondaryDisplay.textContent += (' ' + currentVal + ' ' + operator.textContent);
+            mainDisplay.textContent = operate(Number(prev), Number(currentVal), operation);
+            prev = '';
+            operation = '';
+            isResult = true;
+        } else {
+            isResult = false;
+            if (prev) {
+                prev = operate(Number(prev), Number(currentVal), operation);
+                operation = operator.id;
+
+            } else {
+                prev = currentVal;
+                operation = operator.id;
+            }
+
+            secondaryDisplay.textContent = prev + ' ' + operator.textContent;
+            mainDisplay.textContent = '';
+        }
+    })
+});
+
+const helpers = document.querySelectorAll('.helper');
+
+helpers.forEach(helper => {
+    helper.addEventListener('click', () => {
+        helperFunctions[helper.id]();
+    })
+});
